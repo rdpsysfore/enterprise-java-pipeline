da8d8a17b002ee4e14dc40394cee103beb2e92e7
rdpsysfore <rdpksysfore@gmail.com>
2026-06-25 16:43:20 +0530

    Create App.java

---
+++ src/main/java/com/company/App.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Enterprise Pipeline Demo");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
